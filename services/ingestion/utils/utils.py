92631ed8dffbc5e3e92c239c610a521fb680aca5
S-a-t-y-aa-y-t-a-S <satyasish07@gmail.com>
2026-05-04 23:56:16 +0530

    added readme file to track records

--- services/ingestion/utils/utils.py
+++ services/ingestion/utils/utils.py
@@ -5,20 +5,21 @@ import shutil, os
 from configs.ingestion_configs import ExceptionConfig
 
 def file_handling(uploaded_file: fastapi.UploadFile|str, exception_config: ExceptionConfig):
-    print(type(uploaded_file))
-    if not isinstance(uploaded_file, fastapi.UploadFile):
-        raise ValueError("Provided object is not an UploadFile")
+    
+    try:
+        file_path = str(os.path.join(tempfile.gettempdir(), uploaded_file.filename))
 
-    file_path = str(os.path.join(tempfile.gettempdir(), uploaded_file.filename))
+        with open(file=file_path,
+                mode=constants.WRITE_FILE_MODE_BINARY) as buffer:
+            shutil.copyfileobj(
+                fsrc=uploaded_file.file,
+                fdst=buffer
+            )        
+            return file_path
 
-    with open(file=file_path,
-            mode=constants.WRITE_FILE_MODE_BINARY) as buffer:
-        shutil.copyfileobj(
-            fsrc=uploaded_file.file,
-            fdst=buffer
-        )        
-
-        return file_path
+    except Exception as exp:
+        print(exception_config.saving_file, exp)
+        
 
         
         
