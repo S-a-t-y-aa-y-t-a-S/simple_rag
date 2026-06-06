00a348af8a0a47868f0ea655198d334284c681e1
S-a-t-y-aa-y-t-a-S <satyasish07@gmail.com>
2026-06-06 19:40:06 +0530

    committed to main

--- lab/react_tut/13-notes-app/src/components/RecentTasks.jsx
+++ lab/react_tut/13-notes-app/src/components/RecentTasks.jsx
@@ -10,24 +10,24 @@ const RecentTasks = () => {
     <div className='h-full md:w-1/2 ml-15 mt-15'>
         <h2 className='font-extrabold text-3xl'>Recent Tasks</h2>
         <div className='h-full overflow-y-auto'>
-            <div className='m-3 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
-            <div className='m-3 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
-            <div className='m-3 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
-            <div className='m-3 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
-            <div className='m-3 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
-            <div className='m-3 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
-            <div className='m-3 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
-            <div className='m-3 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
-            <div className='m-3 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
-            <div className='m-3 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
-            <div className='m-3 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
-            <div className='m-3 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
-            <div className='m-3 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
-            <div className='m-3 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
-            <div className='m-3 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
-            <div className='m-3 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
-            <div className='m-3 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
-            <div className='m-3 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
+            <div className='m-2 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
+            <div className='m-2 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
+            <div className='m-2 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
+            <div className='m-2 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
+            <div className='m-2 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
+            <div className='m-2 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
+            <div className='m-2 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
+            <div className='m-2 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
+            <div className='m-2 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
+            <div className='m-2 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
+            <div className='m-2 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
+            <div className='m-2 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
+            <div className='m-2 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
+            <div className='m-2 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
+            <div className='m-2 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
+            <div className='m-2 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
+            <div className='m-2 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
+            <div className='m-2 h-50 w-50 bg-black text-white inline-flex justify-center items-center rounded-3xl shadow-2xl'>RecentTasks</div>
         </div>
         
     </div>
